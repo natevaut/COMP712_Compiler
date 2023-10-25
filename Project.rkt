bbbe8cdf60c8e1abab7603dbed59ed0c33439dab
nate <jmy3092@autuni.ac.nz>
2023-10-25 20:51:10 +1300

    Complete nonrecursive grammar elements

--- Project.rkt
+++ Project.rkt
@@ -12,13 +12,6 @@
     (whitespace (whitespace) skip)
     (Comments ("//" (arbno (not #\newline))) skip)
 
-    ;Keywords
-    (const ("const") symbol)
-    (function ("function") symbol)
-    (return ("return") symbol)
-    (if ("if") symbol)
-    (else ("else") symbol)
-
     ;Expressions
     (number ((arbno digit)) number)
     (boolean ((or "true" "false")) string)
@@ -28,14 +21,6 @@
     (binary-operator (
         (or "+" "-" "*" "/" "%" "===" "!==" ">" "<" ">=" "<=")
     ) symbol)
-
-    ; Punctuation
-    (equals ("=") symbol)
-    (semi (";") symbol)
-    (lparen ("(") symbol)
-    (rparen (")") symbol)
-    (lbrace ("{") symbol)
-    (rbrace ("}") symbol)
     
     ; Variables/user-defined
     (quoted-string ("\"" (arbno (not #\")) "\"") string)
@@ -54,35 +39,49 @@
     ;Program root
     (program (statement) Program)
 
-    ;Blocks
-    (block (lbrace statement rbrace) code-block)
-    (if-block (if lparen expression lparen block) if-block-braced)
-    ;(if-block (if "(" statement ")" statement) if-block-unbraced)
-    (else-block (else block) else-block-braced)
-    ;(else-block (else expression) else-block-unbraced)
-    ;(else-block (else if-block) else-if-block)
-    (conditional-statement (if-block) condition-if)
-    ;(conditional-statement (if-block else-block) condition-if-else)
-
-    ; Statements
-    (statement (expression semi) simple-statement)
+    ; Statements (a line of code)
+    (statement ("const" identifier "=" expression ";")
+       const-declaration)
+    (statement ("function" identifier "(" names ")" block)
+       function-declaration)
+    (statement ("return" expression ";")
+       return-statement)
+    (statement (conditional-block) conditional-statement)
+    (statement (block) block-statement)
+    (statement (expression ";") expression-statement)
     
-    ;Expressions
-    ;(expression (identifier) name)
-    ;(expression (expression binary-operator expression)
-    ;   binary-expression)
-    ;(statement (unary-operator expression)
-    ;    Unary-operator-combination)
+    ;names
+    (names (identifier) one-name)
+    ;TODO many names
 
-    ;Expressions
-    (expression (number) literal)
+    ;Blocks
+    (block ("{" statement "}") code-block)
+     (if-header ("if" "(" expression "(") if-block-header)
+     (if-block (if-header block) if-block-braced)
+     (else-block ("else" block) else-block-braced)
+     (else-block () else-block-empty)
+     (else-if-block ("else" if-block) else-if-block-braced)
+     (conditional-block (if-block else-block) if-else-block)
+    
+    ;Atomic expressions
+    (expression (number) number)
+    (expression (boolean) boolean)
+    (expression (quoted-string) string)
+    (expression (null) null)
+    (expression (identifier) name-expression)
+    ;TODO bin operator combin
+    (expression (unary-operator expression)
+       unary-operator-combination)
+    ;TODO logical composition
+    ;TODO function application
+    ;TODO lambda expression
+    ;TODO lambda expression-2
+    ;TODO conditional expression ?:
+    (expression ("(" expression ")")
+       parenthetical-expression)
 
-    ;Primitives
-    (primitive (number) primitive-number)
-    (primitive (boolean) primitive-boolean)
-    (primitive (quoted-string) primitive-string)
-    (primitive (null) primitive-list)
-    (primitive (binary-logical) primitive-logical)
+    ;expressions
+    ;TODO expressions
   )
 )
 
@@ -99,6 +98,6 @@
 (define just-scan
     (sllgen:make-string-scanner lang-lexical-spec lang-grammar))
 
-(define stmt1 "1;")
+(define stmt1 "-1;")
 
 (display (scan&parse stmt1))
